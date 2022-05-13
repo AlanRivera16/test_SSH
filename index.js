729812f43206085afff24176bd542a03fc236370
Alan.Rivera <rivera.alan.3m@gmail.com>
2022-05-12 19:48:23 -0500

    Merge branch testing into develop

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 function helloWorld(){
-<<<<<<< HEAD
     console.log("Hola mundo");
 }
 
 function develop(){
     console.log("Develop");
-=======
     console.log("Hola mundo ");
 }
 
 function testing(){
     console.log("Testing");
->>>>>>> testing
 }
